cbfcc75c92ff40e90494c2683e9bffcfed591e17
Gaceusz <inglorion@op.pl>
2017-05-05 15:09:07 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var	triangle1Area = getTriangleArea(10, 6);
-	triangle2Area = getTriangleArea(10, -6);
-	triangle3Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(10, -6);
+var triangle3Area = getTriangleArea(10, 15);
+
 function getTriangleArea(a, h) {
 	if ( (a > 0) && (h > 0) ) {
 		return a * h / 2;
@@ -8,6 +9,7 @@ function getTriangleArea(a, h) {
 		return 'Nieprawidłowe dane';
 	}
 }
+
 console.log('Pole pierwszego trójkąta wynosi: ' + triangle1Area);
 console.log('Pole drugiego trójkąta wynosi: ' + triangle2Area);
 console.log('Pole trzeciego trójkąta wynosi: ' + triangle3Area);
